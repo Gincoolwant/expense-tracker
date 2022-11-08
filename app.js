93b2a6bd96a5095ed2e23b30be2aa532b13a57c4
Gincoolwant <soulbox790326@gmail.com>
2022-11-08 20:36:26 +0800

    feat: main page via handlebars

--- app.js
+++ app.js
@@ -1,20 +1,24 @@
 const express = require('express')
-const app = express()
+const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://alpha:camp@cluster0.0c2yjou.mongodb.net/todo-list?retryWrites=true&w=majority')
 const db = mongoose.connection
 
-db.on('error',() => {
+db.on('error', () => {
   console.log('mongodb error')
 })
 
-db.once('open',() => {
+db.once('open', () => {
   console.log('mongodb is connecting')
 })
 
+const app = express()
+
+app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.get('/', (req, res) => {
-  res.send('ok')
+  res.render('index')
 })
 
 const port = 3000
