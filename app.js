94a4172dcb16e08a0aa7424ee1e36170c9300996
Gincoolwant <soulbox790326@gmail.com>
2022-11-09 11:19:02 +0800

    feat: create & delete feature

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://alpha:camp@cluster0.0c2yjou.mongodb.net/todo-list?retryWrites=true&w=majority')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+mongoose.connect('mongodb+srv://alpha:camp@cluster0.0c2yjou.mongodb.net/expense-tracker?retryWrites=true&w=majority')
 const db = mongoose.connection
 
 db.on('error', () => {
@@ -12,13 +14,50 @@ db.once('open', () => {
   console.log('mongodb is connecting')
 })
 
+const Record = require('./models/record.js')
+const CATEGORY = {
+  家居物業: "https://fontawesome.com/icons/home?style=solid",
+  交通出行: "https://fontawesome.com/icons/shuttle-van?style=solid",
+  休閒娛樂: "https://fontawesome.com/icons/grin-beam?style=solid",
+  餐飲食品: "https://fontawesome.com/icons/utensils?style=solid",
+  其他: "https://fontawesome.com/icons/pen?style=solid"
+}
+
 const app = express()
 
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.get('/', (req, res) => {
-  res.render('index')
+  return Record.find()
+    .lean()
+    .then(records => {
+        records.forEach(record => {
+          record.date = record.date.toLocaleDateString()
+        })
+        res.render('index', { records })
+    })
+    .catch(error => console.log(error))
+})
+
+app.get('/new', (req, res) => {
+  res.render('new')
+})
+
+app.post('/records/new', (req, res) => {
+  const { name, date, category, amount } = req.body
+  return Record.create({ name, date, category, amount })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+
+app.delete('/records/:record_id', (req, res) => {
+  const _id = req.params.record_id
+  return Record.deleteOne({ _id })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
 })
 
 const port = 3000
--- models/record.js
+++ models/record.js
@@ -1,10 +1,6 @@
 const mongoose = require('mongoose')
 const Schema =  mongoose.Schema
 const recordSchema = new Schema({
-  id:{
-    type: Number,
-    required: true    
-  },
   name:{
     type: String,
     required: true
@@ -17,18 +13,18 @@ const recordSchema = new Schema({
     type: Number,
     required: true
   },
-  userId:{
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    index: true,
-    required: true
-  },
-  categoryId: {
-    type: Schema.Types.ObjectId,
-    ref: 'Category',
-    index: true,
-    required: true
-  }
+  // userId:{
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   index: true,
+  //   required: true
+  // },
+  // categoryId: {
+  //   type: Schema.Types.ObjectId,
+  //   ref: 'Category',
+  //   index: true,
+  //   required: true
+  // }
 })
 
 module.exports = mongoose.model('Record', recordSchema)
